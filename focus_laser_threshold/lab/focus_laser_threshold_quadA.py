83d814c044afb938f40822314401dc89ad373dd3
AscanioGalbiati <ascanio.galbiati@me.com>
2025-06-17 15:11:46 +0200

    Added focus laser threshold lab codes

---
+++ focus_laser_threshold/lab/focus_laser_threshold_quadA.py
@@ -0,0 +1,382 @@
+''' 
+This code runs a laser focus scan experiment using translation stages and a digital multimeter (DMM) to measure the photocurrent at different positions. 
+Main characteristics of the code: thresholds implemented for adaptive step sizes in X and Z directions
+'''
+
+import io
+import os
+import time
+import json
+import pathlib
+import pickle
+import datetime
+import numpy as np
+import pyvisa as visa
+import matplotlib.pyplot as plt
+import pyvisa_util as pvu
+import trans_stage as stage
+import switchbox_rf as srf
+import multimeter as dmm
+
+# ============================================================ #
+#                       Hard coded paths                       #
+# ============================================================ #
+
+# Script root path
+root_dir = pathlib.Path(__file__).resolve().parent
+
+# Config JSON path
+json_config_path = '../ascanio/config' #../../../config/FocusLaser'
+json_config_fname = 'focus_laser_config_phasespace_xy_asca.json'
+json_config_fullpath = pathlib.Path((root_dir / json_config_path).resolve(), json_config_fname)
+
+# Data save root path
+data_save_root = pathlib.Path('C:/Users/virgorun/Projects/LISA/daq/experiments/Students/ascanio/data')
+
+# ============================================================ #
+#                       Custom functions                       #
+# ============================================================ #
+
+def datetoday(strfmt):
+    today = datetime.date.today()
+    return today.strftime(strfmt)
+
+def timenow(strfmt):
+    tn = time.localtime()
+    return time.strftime(strfmt, tn)
+
+def getDuration(start, end, strfmt=None):
+    '''
+    Calculate the duration between two date and times
+    '''
+
+    # Use the datetime object to convert and calculate the time difference
+    detla_time = datetime.datetime.strptime(end, strfmt) - datetime.datetime.strptime(start, strfmt)
+    
+    delta_totalseconds = detla_time.total_seconds()
+    
+    delta_year, remain_year = divmod(delta_totalseconds, 31536000)
+    delta_days, remain_days = divmod(remain_year, 86400)
+    delta_hours, remain_hours = divmod(remain_days, 3600)
+    delta_minutes, remain_minutes = divmod(remain_hours, 60)
+    
+    print('Duration: {:02d} Years, {:02d} Days, {:02d} Hours, {:02d} Minutes, {:02d} Seconds\n'.format(int(delta_years),
+                                                                                                 int(delta_days),
+                                                                                                 int(delta_hours),
+                                                                                                 int(delta_minutes),
+                                                                                                 int(remain_minutes)))
+
+    
+    return int(delta_year), int(delta_days), int(delta_hours), int(delta_minutes), int(remain_minutes)
+
+def calcDuration(lenx, leny, navg=1, *args):
+    '''
+    Estimate the duration of the script
+    '''
+    # Start with estimated time of the translation stage move time (assuming 5 seconds)
+    est_time_sec = lenx*leny*10
+    
+    for i in args:
+        est_time_sec += (lenx*leny*i*navg)
+
+    delta_year, remain_year = divmod(est_time_sec, 31536000)
+    delta_days, remain_days = divmod(remain_year, 86400)
+    delta_hours, remain_hours = divmod(remain_days, 3600)
+    delta_minutes, remain_minutes = divmod(remain_hours, 60)
+
+    print('Duration: {:02d} Years, {:02d} Days, {:02d} Hours, {:02d} Minutes, {:02d} Seconds\n'.format(int(delta_year),
+                                                                                                 int(delta_days),
+                                                                                                 int(delta_hours),
+                                                                                                 int(delta_minutes),
+                                                                                                 int(remain_minutes)))
+
+    return int(delta_year), int(delta_days), int(delta_hours), int(delta_minutes), int(remain_minutes)
+
+def erf_fit(x,a,b,c,d):
+    #return a*speci.erfc(b*(x-c))+d
+    return a*speci.erf(b*(x-c))+d
+
+def erfc_fit(x,a,b,c,d):
+    return 1 - erf_fit(x,a,b,c,d)
+
+def beam_waist(x):
+    return (1/(x*np.sqrt(2)))
+    #return np.sqrt(2)/x
+
+def dbm2w(x, rbw):
+    #dbm_Hz = x - 10.0*np.log10(rbw)
+    return 1.0 * (10.0 **(x/10.0))/1E3
+
+def mfunc(x, a, b, c):
+    '''
+    Define the M Squared function
+    '''
+    return a * np.sqrt(1 + ((x-c)**2 * 1056E-9**2 * b**4)/(np.pi**2 * a**4))
+
+def mfunc2(x, a, c):
+    '''
+    Define the M Squared function with fixed M^2
+    '''
+    return a * np.sqrt(1 + ((x-c)**2 * 1056E-9**2 * 1.0**4)/(np.pi**2 * a**4))
+
+# ============================================================ #
+#                       Config experiment                      #
+# ============================================================ #
+
+# -------------------- Load config -------------------- #
+with open(json_config_fullpath, 'r') as fopen:
+    focuslaser_jsonconfig = json.load(fopen)
+
+# -------------------- Initialise PyVISA -------------------- #
+rm = visa.ResourceManager()
+
+# -------------------- DMM config -------------------- #
+if 'dmm00' in focuslaser_jsonconfig.keys():
+
+    # Connect to the arbitrary waveform generator
+    dmm00 = rm.open_resource(focuslaser_jsonconfig['dmm00']['conn']['address'], access_mode=0, send_end=True)
+
+    # Reset the instrument
+    dmm00.write("*RST")
+    
+    # Send the list of commands to configure the instrument
+    print('\n-------------------- Configuring Digital Multimeter (DMM) 00 --------------------')
+    pvu.pyvisa_cmd_list(dmm00, focuslaser_jsonconfig['dmm00']['cmd'], 'w')
+
+    # Voltage test measurement
+    dmm00.write("CONF:VOLT:DC")
+    
+    voltage = float(dmm00.query("READ?"))
+    print(f"Measured Voltage: {voltage:.2f} V")
+
+    # Current test measurement
+    dmm00.write("CONF:CURR:DC")
+    
+    current = float(dmm00.query("READ?"))
+    print(f"Measured Photocurrent: {current:.7f} A")
+
+# -------------------- Z stage config -------------------- #
+if 'lta00' in focuslaser_jsonconfig.keys():
+    print('\n-------------------- Configuring (LTA) 00 --------------------')
+    lta00 = stage.conex.connect(rm,
+                                focuslaser_jsonconfig['lta00']['conn']['address'],
+                                focuslaser_jsonconfig['lta00']['conn']['baud_rate'],
+                                focuslaser_jsonconfig['lta00']['conn']['channel'],
+                                focuslaser_jsonconfig['lta00']['conn']['query_delay'],
+                                focuslaser_jsonconfig['lta00']['conn']['force'])
+    # Force home movement. Comment out line if not want to go home. TODO, fix COEX homing.
+    lta00.write('1OR') 
+    zpos = stage.conex.move(lta00, 1, float(focuslaser_jsonconfig['global_params']['zstart_um']), 'abs')
+
+# -------------------- NSC stage config -------------------- #
+if 'nsc00' in focuslaser_jsonconfig.keys():
+    print('\n-------------------- Configuring Newport NewStep Translation Stages (NSC) 00 --------------------')
+    nsc00 = stage.nsc.connect(rm,
+                              focuslaser_jsonconfig['nsc00']['conn']['address'],
+                              focuslaser_jsonconfig['nsc00']['conn']['baud_rate'],
+                              focuslaser_jsonconfig['nsc00']['conn']['query_delay'],
+                              focuslaser_jsonconfig['nsc00']['conn']['timeout'])
+    print(stage.nsc)
+    print(dir(stage.nsc))
+
+    try:
+        stage.nsc.move(nsc00, 7, 'home')
+        #stage.nsc.move(nsc00, 2, 'home')
+    except AttributeError:
+        print("Error: 'nsc_move' method not found.")
+
+    # Move stages to starting position
+    xpos = stage.nsc.move(nsc00, 7, focuslaser_jsonconfig['global_params']['xstart_um'])
+    ypos = stage.nsc.move(nsc00, 2, focuslaser_jsonconfig['global_params']['ystart_um'])
+
+# -------------------- Configure scan parameters -------------------- #
+# Adaptive X step sizes
+xstart = focuslaser_jsonconfig['global_params']['xstart_um']
+xstop = focuslaser_jsonconfig['global_params']['xstop_um']
+xstep_big = focuslaser_jsonconfig['global_params']['xstep_big_um'] 
+xstep_fine = focuslaser_jsonconfig['global_params']['xstep_fine_um']
+x_threshold_start = focuslaser_jsonconfig['global_params']['x_threshold_start_um']
+x_threshold_stop = focuslaser_jsonconfig['global_params']['x_threshold_stop_um']
+
+# Generate x_array with adaptive steps
+x_array = []
+x_current = xstart
+while x_current <= xstop:
+    if x_threshold_start <= x_current <= x_threshold_stop:
+        x_array.append(x_current)
+        x_current += xstep_fine
+    else:
+        x_array.append(x_current)
+        x_current += xstep_big
+x_array = np.array(x_array)
+
+# Y scan parameters
+ystart = focuslaser_jsonconfig['global_params']['ystart_um']
+ystop = focuslaser_jsonconfig['global_params']['ystop_um']
+ystep = focuslaser_jsonconfig['global_params']['ystep_um']
+step_y = int((ystop - ystart) / ystep) + 1
+y_array = np.linspace(ystart, ystop, step_y)
+
+# Adaptive Z step sizes
+zstart = focuslaser_jsonconfig['global_params']['zstart_um']
+zstop = focuslaser_jsonconfig['global_params']['zstop_um']
+zstep_big = focuslaser_jsonconfig['global_params']['zstep_coarse_um']
+zstep_fine = focuslaser_jsonconfig['global_params']['zstep_fine_um']
+z_threshold_start = focuslaser_jsonconfig['global_params']['z_threshold_start_um']
+z_threshold_stop = focuslaser_jsonconfig['global_params']['z_threshold_stop_um']
+
+# -------------------- Save location config -------------------- #
+# Data save location
+save_dir_base = pathlib.Path('{}/{}/GapScan/{}_{}_{}_{}_{}_{}_250617_focus_laser_thres_LB1471C_quadA'.format(
+    data_save_root, 
+    focuslaser_jsonconfig['dut']['batch'],
+    focuslaser_jsonconfig['dut']['foundry'],
+    focuslaser_jsonconfig['dut']['wafer'],
+    focuslaser_jsonconfig['dut']['dut_type'],
+    focuslaser_jsonconfig['dut']['size'],
+    focuslaser_jsonconfig['dut']['gap'],
+    focuslaser_jsonconfig['dut']['dut_id']
+))
+
+# Check the save path and make a folder if it doesn't exist.
+if not save_dir_base.exists():
+    save_dir_base.mkdir(parents=True, exist_ok=True)
+
+save_fname_str = "{}_{}_{}_{}_{}_{}_{}_QPDGapScan_KnifeEdge_250617_focus_laser_thres_LB1471C_quadA".format(
+    focuslaser_jsonconfig['dut']['batch'],
+    focuslaser_jsonconfig['dut']['foundry'],
+    focuslaser_jsonconfig['dut']['wafer'],
+    focuslaser_jsonconfig['dut']['dut_type'],
+    focuslaser_jsonconfig['dut']['gap'],
+    focuslaser_jsonconfig['dut']['gap'],
+    focuslaser_jsonconfig['dut']['dut_id']
+)
+
+# Define the file extension
+fext = '.pkl'
+
+#print('-----------------------------------------------------------------------------------------------------------------------')
+print("Base save directory: {:>100}".format(save_dir_base.as_posix()))                                  
+print("Save filename: {:>78}".format(save_fname_str))
+#print('-----------------------------------------------------------------------------------------------------------------------')
+print('\n-------------------- Setup Complete! --------------------\n')
+
+# ============================================================ #
+#                         Main experiment                      #
+# ============================================================ #
+
+for y in y_array:
+    # Move Y stage
+    try:
+        ypos = stage.nsc.move(nsc00, 2, y)
+    except visa.VisaIOError as e:
+        print(f"Error moving Y stage to {y} um: {e}")
+        nsc00.clear()
+        time.sleep(1)
+        continue
+
+    # Create subfolder for this Y position
+    save_dir_str = save_dir_base / f"Y{y:04.0f}um"
+    if not save_dir_str.exists():
+        save_dir_str.mkdir(parents=True, exist_ok=True)
+
+    # Reset Z position
+    zpos = focuslaser_jsonconfig['global_params']['zstart_um']
+    stage.conex.move(lta00, 1, float(zpos), 'abs')
+
+    while zpos >= zstop:
+        zstep = zstep_fine if z_threshold_start >= zpos >= z_threshold_stop else zstep_big
+
+        # Configure data storage
+        data = {
+            'rawdata': {
+                'comp_gps_time': np.zeros_like(x_array),
+                'stage_laser_xposition': np.zeros_like(x_array),
+                'stage_laser_yposition': np.zeros_like(x_array),
+                'stage_laser_zposition': np.array([zpos]),
+                'quadA': {
+                    'dmm00_curr_amp': np.zeros((len(x_array), focuslaser_jsonconfig['global_params']['number_averages']))
+                }
+            }
+        }
+        
+        data['rawdata']['start_date'] = datetoday('%Y%m%d')
+        data['rawdata']['start_time'] = timenow('%H%M%S')
+
+        # Move Z stage to current position
+        stage.conex.move(lta00, 1, float(zpos), 'abs')
+        data['rawdata']['stage_laser_zposition'] = np.append(data['rawdata']['stage_laser_zposition'], zpos)
+        
+        for idx, x in enumerate(x_array):
+            # Move X stage
+            try:
+                xpos = stage.nsc.move(nsc00, 7, x)
+            except visa.VisaIOError as e:
+                xpos = stage.nsc.move(nsc00, 1, float(x))
+
+            # Update the x position
+            data['rawdata']['stage_laser_xposition'][idx] = xpos
+
+            # Update the y position
+            data['rawdata']['stage_laser_yposition'][idx] = ypos
+
+            # Measure DC photocurrent for Quadrant A with numavg single measurements
+            try:
+                navg = focuslaser_jsonconfig['global_params']['number_averages']
+                temp_measurements = np.zeros(navg)
+                for i in range(navg):
+                    try:
+                        dmm00.write("INIT")
+                        time.sleep(0.05)
+                        temp_measurements[i] = dmm00.query_ascii_values("FETC?")[0]
+                    except Exception as e:
+                        print(f"Error in single measurement {i+1} at X={x} um, Z={zpos} um: {e}")
+                        temp_measurements[i] = np.nan
+                data['rawdata']['quadA']['dmm00_curr_amp'][idx] = temp_measurements
+                print(f"Y={y} um, Z={zpos} um, X={x} um, DC photocurrent = {np.mean(temp_measurements):.7f} A")
+            except Exception as e:
+                print(f"Error measuring at Y={y} um, Z={zpos} um, X={x} um: {e}")
+                data['rawdata']['quadA']['dmm00_curr_amp'][idx] = np.nan
+
+        print(f"DC photocurrent = {np.mean(temp_measurements)}")
+
+        # Save data
+        fout = '{}_{}_{}_Y{:04d}um_Z{:05d}um'.format(
+            data['rawdata']['start_date'],
+            data['rawdata']['start_time'],
+            save_fname_str,
+            int(y),
+            int(np.round(zpos, decimals=3) * 1000)
+        )
+
+        # Combine dictionaries
+        outdict = focuslaser_jsonconfig | data
+
+        # Save the instrument data 
+        try:
+            with open(pathlib.Path(save_dir_str, fout).with_suffix(fext), 'wb') as fopen:
+                pickle.dump(outdict, fopen)
+                print(f"Saving data...\nLocation: {save_dir_str}\nFile Name: {fout}")
+        except Exception as e:
+            print(f"Error saving data: {e}")
+            with open(pathlib.Path(fout).with_suffix(fext), 'wb') as fopen:
+                pickle.dump(outdict, fopen)
+                print("Saving data...\nLocation: {}\nFile Name: {}".format(os.getcwd(), fout))
+
+        # Plot data
+        plt.plot(x_array, data['rawdata']['quadA']['dmm00_curr_amp'].mean(axis=1), label='quadA')
+        plt.legend()
+        plt.title(f'DC Photocurrent at Y={y} um, Z={zpos:.3f} mm')
+        plt.xlabel('X Position (um)')
+        plt.ylabel('Photocurrent (A)')
+        plt.show()
+        
+        zpos -= zstep
+            
+# -------------------- Cleanup -------------------- #
+stage.conex.move(lta00, 1, float(focuslaser_jsonconfig['global_params']['zstart_um']), 'abs')
+stage.nsc.move(nsc00, 7, focuslaser_jsonconfig['global_params']['xstart_um'])
+stage.nsc.move(nsc00, 2, focuslaser_jsonconfig['global_params']['ystart_um'])
+rm.close()
+
+print("Experiment complete!")
